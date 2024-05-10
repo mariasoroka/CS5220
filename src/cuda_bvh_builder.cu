f7d762f2b6baddaeb3ee3c783379d6e307fb57f2
Mateo Guynn <mjguynn@users.noreply.github.com>
2024-05-09 22:27:13 -0400

    Small refactoring/prep

--- src/cuda_bvh_builder.cu
+++ src/cuda_bvh_builder.cu
@@ -6,11 +6,21 @@ constexpr T NP2(T n, T p) {
     return ((n) + (p-1)) / p;
 }
 
+
 struct DeviceUniforms {
     int numTris;
     triangle* __restrict__ tris;
-    float4* __restrict__ triCentroids;
-    float4* __restrict__ triMins;
+
+    // We use float4 rather than a float3 because you can load a float4 with one
+    // instruction. If every thread in a warp loads a consecutive float4, all
+    // 128 bytes can be loaded in a single memory transaction. So with
+    // mins/indices in one buffer and maxs in another, we can load triangle data
+    // across a warp with two total memory transactions.
+
+    // [minX minY minZ (bitcast idx)]
+    float4* __restrict__ triMinsIds;
+
+    // [maxX maxY maxZ (undefined)]
     float4* __restrict__ triMaxs;
 };
 
@@ -21,8 +31,7 @@ public:
 
     ~CudaBVH() {
         cudaFree(_u.tris);
-        cudaFree(_u.triCentroids);
-        cudaFree(_u.triMins);
+        cudaFree(_u.triMinsIds);
         cudaFree(_u.triMaxs);
     }
 private:
@@ -30,31 +39,21 @@ private:
 };
 
 __global__ void setupTris(DeviceUniforms u) {
-    const float oneThird = 1.0f / 3.0f;
-
     int id = blockIdx.x*blockDim.x + threadIdx.x;
     if (id >= u.numTris) return;
 
     triangle& tri = u.tris[id];
-    float3 p1 = {tri.p1.x, tri.p1.y, tri.p1.z};
-    float3 p2 = {tri.p2.x, tri.p2.y, tri.p2.z};
-    float3 p3 = {tri.p3.x, tri.p3.y, tri.p3.z};
-
-    float4 centroid = {
-        oneThird * (p1.x + p2.x + p3.x),
-        oneThird * (p1.y + p2.y + p3.y),
-        oneThird * (p1.z + p2.z + p3.z),
-        0.0f
-    };
-    u.triCentroids[id] = centroid;
+    float3 p1 = {float(tri.p1.x), float(tri.p1.y), float(tri.p1.z)};
+    float3 p2 = {float(tri.p2.x), float(tri.p2.y), float(tri.p2.z)};
+    float3 p3 = {float(tri.p3.x), float(tri.p3.y), float(tri.p3.z)};
 
     float4 min = {
         fminf(p1.x, fminf(p2.x, p3.x)),
         fminf(p1.y, fminf(p2.y, p3.y)),
         fminf(p1.z, fminf(p2.z, p3.z)),
-        0.0f
+        __int_as_float(id)
     };
-    u.triMins[id] = min;
+    u.triMinsIds[id] = min;
 
     float4 max {
         fmaxf(p1.x, fmaxf(p2.x, p3.x)),
@@ -74,8 +73,7 @@ std::shared_ptr<CudaBVH> build_cuda_bvh(
 
     u.numTris = numTris;
     cudaMalloc((triangle**)&u.tris, numTris*sizeof(triangle));
-    cudaMalloc((float4**)&u.triCentroids, numTris*sizeof(float4));
-    cudaMalloc((float4**)&u.triMins, numTris*sizeof(float4));
+    cudaMalloc((float4**)&u.triMinsIds, numTris*sizeof(float4));
     cudaMalloc((float4**)&u.triMaxs, numTris*sizeof(float4));
 
     // Copy triangles into device memory
